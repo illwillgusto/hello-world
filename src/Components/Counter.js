2eaa6f80aca56528a2786f37a04558c54ab298eb
Will Walton <will@endless3dge.com>
2024-04-01 07:35:56 -0500

    Update Counter.js
    implemented prevState because we changed the increment to incrementFive which will allow the increment button to count in sets of 5
    the prevState doesn't use the current state but instead uses the previous state which will allow you to count differently or in a real world scenario to change based on the previous state

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -16,17 +16,29 @@ class Counter extends Component {
     // } // never modify the state directly, this will only show the increment change in the clg 
 
     increment() {
-        this.setState({ // the setState helps avoid the issue above, allows you not to directly modify the state 
-            count: this.state.count + 1
-        }, () => {console.log('Callback value', this.state.count)})
+        // this.setState({ // the setState helps avoid the issue above, allows you not to directly modify the state 
+        //     count: this.state.count + 1
+        // }, () => {console.log('Callback value', this.state.count)})
+
+        this.setState((prevState) => ({ // prevState allows us to use the previous state and not the current state like above
+            count: prevState.count + 1 // this will give us the results to have the increment count in sets of 5
+        }))
         console.log(this.state.count)
+    }
+
+    incrementFive() {
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
 
   render() {
     return (
       <div>
        <div>Count - {this.state.count}</div> 
-       <button onClick={() => this.increment()}>Increment</button>
+       <button onClick={() => this.incrementFive()}>Increment</button>
       </div>
     )
   }
