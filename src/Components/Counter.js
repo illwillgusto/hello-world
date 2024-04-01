29ac2b0e8340ae1d171b6cd7fc6b36e4259bbcc3
Will Walton <will@endless3dge.com>
2024-04-01 07:25:23 -0500

    Update Counter.js
    resolved the increment issue
    NEVER modify the state directly
    instead use setState
    now when the user selects the increment button they will see the count increase in the browser and the console log

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -10,8 +10,15 @@ class Counter extends Component {
       }
     }
     
+    // increment() {
+    //     this.state.count = this.state.count + 1
+    //     console.log(this.state.count)
+    // } // never modify the state directly, this will only show the increment change in the clg 
+
     increment() {
-        this.state.count = this.state.count + 1
+        this.setState({ // the setState helps avoid the issue above, allows you not to directly modify the state 
+            count: this.state.count + 1
+        })
         console.log(this.state.count)
     }
 
