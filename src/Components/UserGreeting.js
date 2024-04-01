2ed5464f6cbc27b66acff00bf67657d0c4231462
Will Walton <will@endless3dge.com>
2024-04-01 10:55:05 -0500

    Update UserGreeting.js
    syntax for the conditional ternary operator which is used more common
    
    syntax for the short-circuit operator as well

--- src/Components/UserGreeting.js
+++ src/Components/UserGreeting.js
@@ -11,15 +11,32 @@ class UserGreeting extends Component {
     }
     
   render() {
-        if (this.state.isLoggedIn) {
-            return (
-                <div>Welcome Will</div>
-            )
-        } else {
-            return (
-                <div>Welcome Guest</div>
-            )
-        }
+
+    return this.state.isLoggedIn && <div>Welcome Will</div>  // approach #4 short-circuit operator
+
+    // return (
+    //     this.state.isLoggedIn ? <div>Welcome Will</div> : <div>Welcome Guest</div>
+    // ) // approach #3, this is shorter to code and cleaner, remember in JS '?' means then, ':' means else, conditional ternary operator
+
+    // let message // element variable approach #2
+    // if (this.state.isLoggedIn) {
+    //     message = <div>Welcome Will</div>
+    // } else {
+    //     message = <div>Welcome Guest</div>
+    // } 
+
+    // return <div>{message}</div>
+
+
+        // if (this.state.isLoggedIn) {
+        //     return (
+        //         <div>Welcome Will</div>
+        //     )
+        // } else {
+        //     return (
+        //         <div>Welcome Guest</div>
+        //     )
+        // } // approach #1
     // return (
     //     <div>
     //         <div>Welcome Will</div>
