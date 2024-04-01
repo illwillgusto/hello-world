18ca52db7d65474bffebce0105ec81a0c475675a
Will Walton <will@endless3dge.com>
2024-04-01 07:40:58 -0500

    Update Counter.js
    added note comments need to knows for setState method

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -45,3 +45,11 @@ class Counter extends Component {
 }
 
 export default Counter
+
+
+/*setState
+-Always make use of setSate and never modify the state directly 
+-Code has to be executed after the state has been updated? Place that ode in the call back 
+function () => {console.log('Callback value')}, which is the second argument to the setState method.
+-When you have to update state based on the previous state value, pass in a function as an 
+argument instead of the regular object */
