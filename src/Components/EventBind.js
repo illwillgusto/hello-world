3fdc8d6a2f3896768bc59251da18019ec74a1649
Will Walton <will@endless3dge.com>
2024-04-01 09:50:56 -0500

    Update EventBind.js
    approach #4
    arrow function as a class property

--- src/Components/EventBind.js
+++ src/Components/EventBind.js
@@ -9,22 +9,28 @@ class EventBind extends Component {
          message: 'Wassup'
       }
 
-      this.clickHandler = this.clickHandler.bind(this) // binding in this approach is recommended 
+    //   this.clickHandler = this.clickHandler.bind(this) // binding in this approach #3 is recommended 
     }
 
-    clickHandler() {
-        this.setState({
-            message: 'Goodbye' // this will work with the assist of the bind method, which was added in the onClick 
-        })
-        console.log(this)
-    }
+    // clickHandler() {
+    //     this.setState({
+    //         message: 'Goodbye' // this will work with the assist of the bind method, which was added in the onClick 
+    //     })
+    //     console.log(this)
+    // }
     
+    clickHandler = () => {
+        this.setState({
+            message: "Goodbye"
+        })
+    } //approach #4
+
   render() {
     return (
       <div>
         <div>{this.state.message}</div>
-        {/* <button onClick={this.clickHandler.bind(this)}>Click</button>  */}
-        {/* <button onClick={() => this.clickHandler()}>Click</button>  */}
+        {/* <button onClick={this.clickHandler.bind(this)}>Click</button>  approach 1*/} 
+        {/* <button onClick={() => this.clickHandler()}>Click</button>  approach 2*/}
         <button onClick={this.clickHandler}>Click</button> 
       </div>
     )
@@ -38,3 +44,4 @@ export default EventBind
 // the function body is a single statement, the event handler is being called and returning that value 
 // this is why you see () after the event handler and are required  
 // approach 3, best approach, binding the event handler in the constructor 
+// approach 4, use an arrow function as a class property 
