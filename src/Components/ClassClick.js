9196ac6a8e32e74caafb7a151ce6eaa0d73fd5b2
Will Walton <will@endless3dge.com>
2024-04-02 12:54:10 -0500

    Update ClassClick.js
    added learning comments for a reminder for my son to review later when handling eventHandlers in class component

--- src/Components/ClassClick.js
+++ src/Components/ClassClick.js
@@ -15,3 +15,5 @@ class ClassClick extends Component {
 
 export default ClassClick
 
+// when handling event handlers in class components keep in mind that you will also declare a function within the function
+// you will also use the 'this' keyword prior to the eventHandler and don't add () to the end, this will be a function call then 
