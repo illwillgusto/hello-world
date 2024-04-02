1ec7f4f0b90bde03243dc8e0e0cfe84e9fe0e2eb
Will Walton <will@endless3dge.com>
2024-04-02 13:50:45 -0500

    Update Person.js
    point of another example of destructing

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Person({person}) {
+function Person({person}) { //another example of destructing the prop right in the parameter
   return (
     <div>
       <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}.</h2>
