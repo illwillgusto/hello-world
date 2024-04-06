328d68cf6ff3f4dcb665dfa3b12302854104eb85
Will Walton <will@endless3dge.com>
2024-04-06 00:02:50 -0500

    Update ComponentNotes.js
    notes added for the Updating phase of the components lifecycle methods

--- src/Components/ComponentNotes.js
+++ src/Components/ComponentNotes.js
@@ -25,11 +25,27 @@ invoked immediately after a component and all its children components have been 
 cause side effects to load data 
 
 Updating phase has 5 methods;
-static getDerivedStateFromProps
-shouldComponentUpdate
-render()
-getSnapshotBeforeUpdate
-componentDidUpdate
+static getDerivedStateFromProps - method is called every time a component is re-rendered 
+used when the state depends on the props of the component. NO SIDE EFFECTS.
+
+shouldComponentUpdate - dictates if the component should re-render or not, by default all class components will 
+re-render whenever the props they receive or their state changes. this method can prevent default behavior 
+by returning false. compare the existing props and state values w/ the next props and state values, and return 
+true or false to let React know whether the component should update of not. Performance optimization.
+NO SIDE EFFECTS
+
+render() - read props & state and return JSX. the JSX describes the UI.
+
+getSnapshotBeforeUpdate - previous props and previous state as its parameters. Called right before the changes from 
+the virtual DOM are to be reflected in the DOM. You could use this method Capture some information from the DO. 
+this method will either return 'null' or a value, that value will be passed as the 3rd parameter to the componentDidUpdate 
+method. 
+
+componentDidUpdate - this method will be called after the render is finished in the re-render cycles. this method 
+accepts 3 parameters; previous props, previous state, the snapshot value returned from getSnapshotBeforeUpdate method.
+this method will only be called once each re-render cycle. you can cause side effects for this method, you can make 
+Ajax calls. before the call you have to compare the previous props with the new props then decide whether to make 
+the Ajax call or not.
 
 Unmounting phase has 1 method:
 componentWillUnmount
