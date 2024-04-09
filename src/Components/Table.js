43cb9f99d8ff147b998e50badb365b515fbd471b
Will Walton <will@endless3dge.com>
2024-04-09 18:52:35 -0500

    2 new components
    created a tables component
    created a columns component

---
+++ src/Components/Table.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Table() {
+  return (
+    <table>
+      <tbody>{/* table body */}
+        <tr> {/* table row */}
+          
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default Table
