13382d76f013c2337c575a5f6de670ac0a5a4651
Will Walton <will@endless3dge.com>
2024-04-09 18:25:41 -0500

    change state button
    change state button added
    added return null to getSnapshotBeforeUpdate for both lifecycle components
    
    declared the chanegState function to setState and gave it a name prop

--- src/Components/LifecycleA.js
+++ src/Components/LifecycleA.js
@@ -27,17 +27,25 @@ class LifecycleA extends Component {
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
       console.log('LifecycleA getSnapshotBeforeUpdate')
+      return null
     }
 
     componentDidUpdate() {
       console.log('LifecycleA componentDidUpdate')
     }
 
+  changeState = () => {
+    this.setState({
+      name: 'Codevolution'
+    })
+  }  
+
   render() {
     console.log('LifecycleA render')
     return (
       <div>
         <div>Lifecycle A</div>
+        <button onClick={this.changeState}>Change state</button>
         <LifecycleB />
       </div>
     )
