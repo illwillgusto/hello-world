661e601f4cef3537b601e144c85dfecdf4a5a0b3
Will Walton <will@endless3dge.com>
2024-04-10 08:36:09 -0500

    Update App.js
    imported ParentComp.js to App.js

--- src/App.js
+++ src/App.js
@@ -21,12 +21,13 @@ import LifecycleA from './Components/LifecycleA';
 import FragmentDemo from './Components/FragmentDemo';
 import Table from './Components/Table';
 import PureComponent from './Components/PureComponent';
+import ParentComp from './Components/ParentComp';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <PureComponent />
+        <ParentComp />
 
 
 
