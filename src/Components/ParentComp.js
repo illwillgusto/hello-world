bc42e750ec13ef9b24c71005312d5ad2f12fb75e
Will Walton <will@endless3dge.com>
2024-04-10 08:23:59 -0500

    Create ParentComp.js
    Parent component created, this will actually be the parent component to ChildComponent.js

---
+++ src/Components/ParentComp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ParentComp extends Component {
+  render() {
+    return (
+      <div>
+        Parent Component
+      </div>
+    )
+  }
+}
+
+export default ParentComp
