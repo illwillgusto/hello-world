3ef992c023d109183e9de9b217384b887ebc3094
Will Walton <will@endless3dge.com>
2024-04-10 08:55:23 -0500

    Update PureComponent.js
    notes on pure component and the use of shallow comparison, also why this is important when dealing with pure components

--- src/Components/PureComponent.js
+++ src/Components/PureComponent.js
@@ -17,4 +17,19 @@ export default PureComp
 /*a pure component implements shouldComponentUpdate method with 
 a shallow props and state comparison 
 
-*/
+Shallow Comparison (SC)
+Primitive Types-
+numbers, strings, and boolean 
+a (SC) b returns true if a and b have the same value and are of the same type
+Ex: string 'Will'(SC) string 'Will' are the same in value and type so the return will be true 
+
+Complex Types-
+objects & arrays 
+a(SC) b returns true if a and b reference the exact same object
+
+Pure Component -
+
+SC of prevState with currentState
+SC of prevProps with currentProps
+so with that only when there is a difference between the prev and current will the component re-render 
+ */
