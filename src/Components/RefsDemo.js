1623658b8d850b6f9f48f6e7c56b2c823bf4c4de
Will Walton <will@endless3dge.com>
2024-04-11 17:56:04 -0500

    Update RefsDemo.js
    added the ref prop to the constructor
    
    then added the ref attribute to the input element with the ref prop from the constructor as it's value
    
    called the focus method through the componentDidMount method
    
    this will allow the input field to be the focus for when the page is loaded

--- src/Components/RefsDemo.js
+++ src/Components/RefsDemo.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
  class RefsDemo extends Component {
+    constructor(props) {
+      super(props)
+      this.inputRef = React.createRef() //this allows for them to be referenced throughout the component 
+    }
+
+    componentDidMount() {
+        this.inputRef.current.focus()
+        console.log(this.inputRef)
+    }
+    
   render() {
     return (
       <div>
-        <input></input>
+        <input type='text' ref={this.inputRef}/>
+
       </div>
     )
   }
@@ -14,4 +25,15 @@ export default RefsDemo
 
 
 /*Refs
-Allow  */
+Make it possible to access Dom nodes directly within React 
+
+1. within the constructor you will create a new property, 'this.inputRef = React.createRef()', this allows for the 
+reference to be referenced throughout the component.
+2. attach the ref to the input element in the JSX, within the render method, you will use the reserve ref attribute =
+ref={the reference prop you declared in the constructor}.
+3. call the focus method on the input element, use the componentDidUpdate method between the constructor and the render,
+you will have to use the following syntax, this.inputRef.current.focus()
+
+
+You can also use refs to fetch the input value 
+  */
