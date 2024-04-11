16cec2431124cf9a940841e250e1f74261524159
Will Walton <will@endless3dge.com>
2024-04-11 17:40:59 -0500

    RefDemo component
    created RefDemo component to begin learning about Refs

---
+++ src/Components/RefsDemo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+ class RefsDemo extends Component {
+  render() {
+    return (
+      <div>
+        <input></input>
+      </div>
+    )
+  }
+}
+
+export default RefsDemo
+
+
+/*Refs
+Allow  */
