cddd93824cccf85cb8cae5f5dcce093c4fad3fec
Will Walton <will@endless3dge.com>
2024-04-12 14:40:47 -0500

    Update PortalDemo.js
    note on why we may use portals

--- src/Components/PortalDemo.js
+++ src/Components/PortalDemo.js
@@ -21,4 +21,7 @@ there's one DOM element in the beginning HTML that we mount the React applicatio
 so when we go to out index.js file we use ReactDom.render and mount our App.js onto the root element.
 
 React portals provide the ability to break out of the normal DOM tree, so you can render a component onto 
-a DOM node that is not under the 'root' element*/
+a DOM node that is not under the 'root' element
+
+Portals can be brought up when dealing with a parent component's CSS when the child component's is a 
+model, pop-up, or tooltip*/
