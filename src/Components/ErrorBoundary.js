e242fcb0b23a1be40ada547729cdb16f42976b2b
Will Walton <will@endless3dge.com>
2024-04-13 18:04:17 -0500

    Update ErrorBoundary.js
    declared the constructor with the state prop hasError

--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react'
 
 class ErrorBoundary extends Component {
 
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         hasError: false
+      }
+    }
+    
     static getDerivedStateFromError(error) {
+
+        
         return {
             hasError: true
         }
