6512d066ad9afd5eae36693607fd4dd3d1e96356
Will Walton <will@endless3dge.com>
2024-04-13 17:49:25 -0500

    Update ErrorBoundary.js
    what the lifecycle methods offer
    getDerivedStateFromError will offer you an error and where the error is
    
    componentDidCatchError will offer you the error and additional info

--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -18,7 +18,7 @@ React components that catch JavaScript error in their child component tree,
 log those errors, and display a fall-back UI.
 
 A class component becomes an Error Boundary by defining either or both of 
-the lifecycle methods; getDerivedStateFromError methods and componentDidCatch method.
+the lifecycle methods; getDerivedStateFromError(error) methods and componentDidCatch(error, info) method.
 
 The placement of the Error Boundary also matters as it controls if the 
 entire app should have the fall-back UI or just the component causing the problem. 
