e27b42e4173ee311b7af0c2bdfd3868ced19d1c0
Will Walton <will@endless3dge.com>
2024-04-13 18:02:53 -0500

    Update ErrorBoundary.js
    defined the getDerivedStateFromError method
    passed 'error' as the para
    
    added returning the new state object
    
    set a prop called hasError and set it to true

--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 class ErrorBoundary extends Component {
+
+    static getDerivedStateFromError(error) {
+        return {
+            hasError: true
+        }
+    }
   render() {
     return (
       <div>
