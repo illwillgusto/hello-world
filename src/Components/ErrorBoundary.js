57ba015368c1c3e18646394a5ed83c82776a2e15
Will Walton <will@endless3dge.com>
2024-04-13 17:47:38 -0500

    Update ErrorBoundary.js
    summary notes for the benefits of error boundaries
    
    what error boundaries are
    
    what lifecycle methods they use
    
    the proper placement of them as well

--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -12,3 +12,16 @@ class ErrorBoundary extends Component {
 
 export default ErrorBoundary
 
+/*Error Boundary 
+
+React components that catch JavaScript error in their child component tree,
+log those errors, and display a fall-back UI.
+
+A class component becomes an Error Boundary by defining either or both of 
+the lifecycle methods; getDerivedStateFromError methods and componentDidCatch method.
+
+The placement of the Error Boundary also matters as it controls if the 
+entire app should have the fall-back UI or just the component causing the problem. 
+
+Provide a way to gracefully handle error in application code. 
+*/
