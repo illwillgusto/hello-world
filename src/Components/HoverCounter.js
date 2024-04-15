2d777296ef6a6b8c07c49a89c96803305a58e693
Will Walton <will@endless3dge.com>
2024-04-15 13:13:21 -0500

    Update HoverCounter.js
    brought the constructor and the handler from ClickCount comp over to this comp

--- src/Components/HoverCounter.js
+++ src/Components/HoverCounter.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 class HoverCounter extends Component {
+    constructor(props) {
+        super(props)
+      
+        this.state = {
+           count: 0
+        }
+      }
+  
+      incrementCount = () => {
+          this.setState( prevState => {
+              return { count: prevState.count + 1 }
+          })
+      }
   render() {
     return (
       <div>
