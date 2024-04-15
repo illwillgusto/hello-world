dc6ef8faf817a2b2346552fe51b785d2582bbc8f
Will Walton <will@endless3dge.com>
2024-04-15 13:39:17 -0500

    duplicate code removed
    removed the duplicate code that's now been added in the HOC from both the click counter comp and the hover counter comp

--- src/Components/ClickCounter.js
+++ src/Components/ClickCounter.js
@@ -2,19 +2,6 @@ import React, { Component } from 'react';
 import withCounter from './withCounter';
 
 class ClickCounter extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         count: 0
-      }
-    }
-
-    incrementCount = () => {
-        this.setState( prevState => {
-            return { count: prevState.count + 1 }
-        })
-    }
     
   render() {
     const { count } = this.state
--- src/Components/HoverCounter.js
+++ src/Components/HoverCounter.js
@@ -2,19 +2,7 @@ import React, { Component } from 'react'
 import withCounter from './withCounter'
 
 class HoverCounter extends Component {
-    constructor(props) {
-        super(props)
-      
-        this.state = {
-           count: 0
-        }
-      }
-  
-      incrementCount = () => {
-          this.setState( prevState => {
-              return { count: prevState.count + 1 }
-          })
-      }
+ 
   render() {
     const {count} = this.state
     return <h2 onMouseOver={this.incrementCount}>{this.props.name} Hover { count } Times</h2>
