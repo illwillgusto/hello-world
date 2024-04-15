e08339e8cbd5d0d5829f3de5775d1955b4dce9fc
Will Walton <will@endless3dge.com>
2024-04-15 13:42:53 -0500

    Update ClickCounter.js
    deconstructed the count and incrementCount props

--- src/Components/ClickCounter.js
+++ src/Components/ClickCounter.js
@@ -4,7 +4,7 @@ import withCounter from './withCounter';
 class ClickCounter extends Component {
     
   render() {
-    const { count } = this.state
+    const { count, incrementCount } = this.props
      return  <button onClick={this.incrementCount}>{this.props.name} Clicked { count } Times</button>
   }
 }
