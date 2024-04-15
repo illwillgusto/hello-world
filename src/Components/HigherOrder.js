b68ccd7af25e3f7d943ec6988019eb9442012852
Will Walton <will@endless3dge.com>
2024-04-15 10:18:31 -0500

    Update HigherOrder.js
    Notes on what an HOC is and what it does
    
    provided examples as well to get a better understanding

--- src/Components/HigherOrder.js
+++ src/Components/HigherOrder.js
@@ -1,2 +1,9 @@
 /*Higher Order Components 
+-helps share common functionality between components without repeating code 
+-HOC is a pattern where a function takes a component as an argument and returns a new component.
+example syntax - const NewComponent = higherOrderComponent(originalComponent)
+-HOC adds additional data or functionality to the original component, so the new component can also be referred to as
+enhanced component.
+example - const IronMan = withSuit(TonyStark);
+
  */
