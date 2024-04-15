17e12779eb70d5374a5f14326568e503b44923b3
Will Walton <will@endless3dge.com>
2024-04-15 13:03:42 -0500

    Update ClickCounter.js
    added a constructor for the state prop count with the starting value 0
    
    clickHandler has been been added to the button with the value of incrementCount

--- src/Components/ClickCounter.js
+++ src/Components/ClickCounter.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 class ClickCounter extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count: 0
+      }
+    }
+    
   render() {
     return (
       <div>
-        <button>Clicked X Times</button>
+        <button onClick={this.incrementCount}>Clicked X Times</button>
       </div>
     )
   }
